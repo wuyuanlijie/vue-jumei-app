3cca7e3368f8a3b43cef98b219c93b9f9a387633
JerryLee <wuyuanlijie@qq.com>
2017-09-25 16:25:21 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/pages/index/index'
-import liveCommunity from '@/pages/livecommunity/index'
-import mySelf from '@/pages/myself/index'
-import login from '@/pages/mySelf/login'
-import setter from '@/pages/mySelf/setter'
-import brandSale from '@/pages/brandsale/index'
-import shopCart from '@/pages/shopcart/index'
-import indexPage1 from '@/pages/index/page1'
-import indexPage2 from '@/pages/index/page2'
-import indexPage3 from '@/pages/index/page3'
-import indexPage4 from '@/pages/index/page4'
-import indexPage5 from '@/pages/index/page5'
-import indexPage6 from '@/pages/index/page6'
-import indexPage7 from '@/pages/index/page7'
-import product from '@/pages/product/index'
-import info from '@/pages/product/info'
-import detail from '@/pages/product/detail'
-import comment from '@/pages/product/comment'
+
 
 //可以使用路由懒加载
 Vue.use(Router)
@@ -27,104 +10,104 @@ export default new Router({
     { //这里要设置一个默认的主页面 后面才起作用 /代表根目录
       path: '/',
       name: 'index',
-      component: index,
+      component: resolve => require(['@/pages/index/index'], resolve),
       redirect: '/index/page1'
     },
-    {
+    { 
       path: '/index',
-      component: index,
+      component: resolve => require(['@/pages/index/index'], resolve),
       meta: {keepAlive: true},
       children: [
         {
           path: '',
-          component: indexPage1
+          component: resolve => require(['@/pages/index/index'], resolve)
         },
         {
           path: 'page1', 
           name: 'page1',
-          component: indexPage1
+          component: resolve => require(['@/pages/index/page1'], resolve)
         },
         {
           path: 'page2',
           name: 'page2',
-          component: indexPage2
+          component: resolve => require(['@/pages/index/page2'], resolve)
         },
         {
           path: 'page3',
           name: 'page3',
-          component: indexPage3
+          component: resolve => require(['@/pages/index/page3'], resolve)
         },
         {
           path: 'page4',
           name: 'page4',
-          component: indexPage4
+          component: resolve => require(['@/pages/index/page4'], resolve)
         },
         {
           path: 'page5',
           name: 'page5',
-          component: indexPage5
+          component: resolve => require(['@/pages/index/page5'], resolve)
         },
         {
           path: 'page6',
           name: 'page6',
-          component: indexPage6
+          component: resolve => require(['@/pages/index/page6'], resolve)
         },
         {
           path: 'page7',
-          component: indexPage7
+          component: resolve => require(['@/pages/index/page7'], resolve)
         },
       ]
     },
     {
       path: '/brandsale',
-      name: 'brandSale',
-      component: brandSale
+      name: 'brandSale', 
+      component: resolve => require(['@/pages/brandsale/index'], resolve)
     },
     {
       path: '/livecommunity',
       name: 'liveCommunity',
-      component: liveCommunity
+      component:  resolve => require(['@/pages/livecommunity/index'], resolve)
     },
     {
       path: '/shopcart',
       name: 'shopCart',
-      component: shopCart
+      component:  resolve => require(['@/pages/shopCart/index'], resolve)
     },
     {
       path: '/myself',
       name: 'mySelf',
-      component: mySelf,
+      component: resolve => require(['@/pages/mySelf/index'], resolve)
     },
     {
       path: "/setter",
       name: 'setter',
-      component: setter
+      component:  resolve => require(['@/pages/mySelf/setter'], resolve)
     },
     {
       path: '/login',
       name: 'login',
-      component: login
+      component:  resolve => require(['@/pages/mySelf/login'], resolve)
     },
     {
       path: '/product',
       name: 'product',
-      component: product,
-      redirect: '/product/info',
+      component:  resolve => require(['@/pages/product/index'], resolve),
+      redirect: '/product/info', 
       children: [
         {
           path: 'info',
           name: 'productInfo',
-          component: info
+          component: resolve => require(['@/pages/product/info'], resolve)
         },
         {
           path: 'detail',
           name: 'productDetail',
-          component: detail
+          component: resolve => require(['@/pages/product/detail'], resolve)
         },
         {
           path: 'comment',
           name: 'productComment',
-          component: comment
+          component: resolve => require(['@/pages/product/comment'], resolve)
         }
       ]
     },
